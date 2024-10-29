0137d62ae7d1a5d33a4dd6045e54edc45a1ffb6f
Artur Leonel <Arturleonel08@outlook.com>
2024-10-29 10:48:57 -0300

    correção de bugs2.0 e leitura do codg

--- app/src/main/java/com/example/artur_leonel_pe_modulo_3/MainActivity.kt
+++ app/src/main/java/com/example/artur_leonel_pe_modulo_3/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.artur_leonel_pe_modulo_3
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -15,23 +16,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val textView7 = findViewById<TextView>(R.id.textView7)
+        val textView6 = findViewById<TextView>(R.id.textView6)
+        
         val userName = intent.getStringExtra("username")
         val email = intent.getStringExtra("email")
         val text = findViewById<TextView>(R.id.textView5)
         text.text = "${updateGreeting()}, $userName"
-        val textView6 = findViewById<TextView>(R.id.textView6)
+
         textView6.text = userName
-        val textView7 = findViewById<TextView>(R.id.textView7)
+
         textView7.text = email
-        val textView8 = findViewById<TextView>(R.id.textView8)
-        textView8.text = "PE"
+        startCountdown(3700000)
     }
 
+    //função para o "Onclick" do imageview
     fun Logout(view: View) {
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 
+    //Função para puxar o horário
     private fun updateGreeting(): String {
         val calendar = Calendar.getInstance()
         val hourOfDay = calendar.get(Calendar.HOUR_OF_DAY)
@@ -44,4 +49,24 @@ class MainActivity : AppCompatActivity() {
         val userName = "Usuário" // Substitua pelo nome do usuário real
         return greeting
     }
+
+    //Função para iniciar cronometro
+    private fun startCountdown(millis: Long) {
+        val tvCountdown = findViewById<TextView>(R.id.cronometro)
+
+        object : CountDownTimer(millis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val hours = (millisUntilFinished / (1000 * 60 * 60)).toInt()
+                val minutes = (millisUntilFinished % (1000 * 60 * 60) / (1000 * 60)).toInt()
+                val seconds = (millisUntilFinished % (1000 * 60) / 1000).toInt()
+
+                tvCountdown.text = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+            }
+
+            override fun onFinish() {
+                tvCountdown.text = "00:00:00"
+            }
+        }.start()
+    }
+
 }
