43b96af61defbfa2a72b64246bfd7ef371ac98ac
Artur Leonel <Arturleonel08@outlook.com>
2024-10-29 10:12:08 -0300

    Login/Cadastro
    Realizado login e cadastro com layout e kotlin

--- app/src/main/java/com/example/artur_leonel_pe_modulo_3/CadastroActivity.kt
+++ app/src/main/java/com/example/artur_leonel_pe_modulo_3/CadastroActivity.kt
@@ -3,18 +3,93 @@ package com.example.artur_leonel_pe_modulo_3
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.content.SharedPreferences
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val nameFull = findViewById<EditText>(R.id.nome_completo_cadastro)
+        val emailUsuario = findViewById<EditText>(R.id.email_usuario)
+        val password = findViewById<EditText>(R.id.senha_cadastro)
+        val confirmPassword = findViewById<EditText>(R.id.confirmar_senha_cadastro)
+        val buttonCadastrarCadastro = findViewById<Button>(R.id.botao_cadastrar_cadastro)
+        val iconVoltar = findViewById<ImageView>(R.id.icon_voltar)
+        iconVoltar.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
+
+        buttonCadastrarCadastro.setOnClickListener {
+            val inputNameFull = nameFull.text.toString()
+            val inputEmail = emailUsuario.text.toString()
+            val inputPassword = password.text.toString()
+            val inputConfirmPassword = confirmPassword.text.toString()
+
+            if (inputNameFull.isBlank() || inputEmail.isBlank() || inputPassword.isBlank() || inputConfirmPassword.isBlank()) {
+                messageShowBlock("Preencha Todos Os Dados!")
+            } else if (!verificarNomeCompleto(inputNameFull)) {
+                messageShowBlock("O Nome Completo deve conter pelo menos dois nomes!")
+            } else if (!validarEmail(inputEmail)) {
+                emailUsuario.error = ("não pode começar com número, deve conter no mínimo 6 caracteres antes do “@”, após o “@” deve conter no mínimo 3 caracteres seguido de “.com” ou “.com.br”")
+            } else if (compararPassword(inputPassword, inputConfirmPassword)) {
+                messageShowBlock("As Senhas Não Conferem")
+            } else if (!validarSenha(inputPassword)) {
+            messageShowBlock("A senha é necessária no mínimo 8 caracteres, no mínimo dois números, uma letra maiuscula e uma letra minuscula.\n")
+            } else {
+                adicionarUser(inputNameFull, inputPassword)
+                messageShowBlock("Cadastro Realizado Com Sucesso!")
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }
+    }
+
+    fun validarEmail(email: String): Boolean {
+        // Regex para validar o e-mail
+        val regex = Regex("^(?![0-9])([a-z0-9_.]{6,})@([a-z0-9]{3,})(\\.com|\\.com\\.br)$")
+        return regex.matches(email)
+    }
+
+    fun validarSenha(senha: String): Boolean {
+        // Verifica o tamanho
+        if (senha.length < 8) return false
+
+        // Contadores para os requisitos
+        val hasUpperCase = senha.count { it.isUpperCase() } > 0
+        val hasLowerCase = senha.count { it.isLowerCase() } > 0
+        val hasTwoDigits = senha.count { it.isDigit() } >= 2
+        val hasValidCharacters = senha.all { it.isLetterOrDigit() || it == '_' || it == '.' }
+
+        // Retorna true se todos os requisitos forem atendidos
+        return hasUpperCase && hasLowerCase && hasTwoDigits && hasValidCharacters
+    }
+
+    //Mensagem toast para o usuario
+    private fun messageShowBlock(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    //Verificar possivel nome e sobrenome
+    private fun verificarNomeCompleto(nomeCompleto: String): Boolean {
+        val palavras = nomeCompleto.trim().split("\\s+".toRegex())
+        return palavras.size >= 2
+    }
+    //comparar as Senha
+    private fun compararPassword(password: String, confirmPassword: String): Boolean {
+        return password != confirmPassword
+    }
+    //Adicionar ao SharedPreferences
+    private fun adicionarUser(userName: String, password: String) {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("PREFS", MODE_PRIVATE)
+        val userApp = sharedPreferences.edit()
+        userApp.putString("username", userName)
+        userApp.putString("password", password)
+        userApp.apply()
     }
 }
--- app/src/main/java/com/example/artur_leonel_pe_modulo_3/LoginActivity.kt
+++ app/src/main/java/com/example/artur_leonel_pe_modulo_3/LoginActivity.kt
@@ -3,18 +3,83 @@ package com.example.artur_leonel_pe_modulo_3
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class LoginActivity : AppCompatActivity() {
+    private var attempCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val botaoAcessar = findViewById<Button>(R.id.acessar_login)
+        val botaoCadastro = findViewById<Button>(R.id.cadastrar_login)
+        val userName = findViewById<EditText>(R.id.username_acessar)
+        val password = findViewById<EditText>(R.id.password_login)
+
+        val backgroundRed = ContextCompat.getDrawable(this, R.drawable.borderred)
+        val backgroudDefault = ContextCompat.getDrawable(this, R.drawable.borderblack)
+
+        botaoCadastro.setOnClickListener {
+            startActivity(Intent(this, CadastroActivity::class.java))
         }
+
+        botaoAcessar.setOnClickListener {
+            val inputUsername = userName.text.toString()
+            val inputPassword = password.text.toString()
+            if (verificarConta(inputUsername, inputPassword) || inputUsername == "admin" && inputPassword == "1q2w3e") {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("username", inputUsername)
+                startActivity(intent)
+            } else {
+                messageShowBlock("Dados Incorretos!!")
+                userName.background = backgroundRed
+                password.background = backgroundRed
+                attempCount++
+                Handler().postDelayed({
+                    if(attempCount>3) {
+                        userName.background = backgroundRed
+                        password.background = backgroundRed
+                    } else {
+                        userName.background = backgroudDefault
+                        password.background = backgroudDefault
+                    }
+                }, 750)
+                if (attempCount>3) {
+                    //Desabilita os Widgets/Views
+                    messageShowBlock("Login bloqueado: aguarde 30s!")
+                    userName.isEnabled = false
+                    password.isEnabled = false
+                    botaoAcessar.isEnabled = false
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        //Habilita as Widgets/View
+                        userName.background = backgroudDefault
+                        password.background = backgroudDefault
+                        userName.isEnabled = true
+                        password.isEnabled = true
+                        botaoAcessar.isEnabled = true
+                        attempCount = 0
+                    }, 10000)
+
+                }
+            }
+        }
+
+    }
+    private fun messageShowBlock(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun verificarConta (usuario: String, senha: String):Boolean {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("PREFS", MODE_PRIVATE)
+        val usuario2 = sharedPreferences.getString("username", null)
+        val senha2 = sharedPreferences.getString("password", null)
+        if (usuario2 == usuario && senha2 == senha) return true else return false
     }
 }
